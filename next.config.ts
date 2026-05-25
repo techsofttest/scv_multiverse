5d7e97f49af1bae41526411a4e866493fa8a75b6
TechSoft <techsofttest@gmail.com>
2026-05-25 14:25:10 +0530

    change in next.config

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,14 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/uploads/**',
+        protocol: 'https',
+        hostname: 'techsoftwebsolutions.com',
+        pathname: '/techsoft/demo/SCV/backend/public/uploads/**',
       },
     ],
-    // This is the key setting for your specific error:
-    dangerouslyAllowSVG: true, 
-    unoptimized: true, // Only use this if you want to skip Next.js image optimization
+
+    dangerouslyAllowSVG: true,
+    unoptimized: true,
   },
 };
 
