f38247481ed12812ad3234c9f29b97ce490e9938
Techsoft Web Solutions <techsofttest@gmail.com>
2026-05-25 13:19:11 +0530

    Refactor next.config.ts to export axios instance

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/uploads/**',
-      },
-    ],
-    // This is the key setting for your specific error:
-    dangerouslyAllowSVG: true, 
-    unoptimized: true, // Only use this if you want to skip Next.js image optimization
-  },
-};
+import axios from "axios";
 
-export default nextConfig;
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
+});
+
+export default api;
